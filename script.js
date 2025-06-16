f67e3692f3f9d46480e6a2ad7b966f3a1b135813
aj1441 <167340937+aj1441@users.noreply.github.com>
2025-06-16 12:54:26 -0700

    end of collab session

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+//Data comes from two places: form & API
+//We can't make the api call until the user fills out the form
+//So first, run the form to start the submit function
+// then, pass data from submit function to api call
+//once we get the API data, we can render the image to the user/display to user
+
 const form = document.querySelector("#nasa-form");
+const result = document.querySelector("#result")
 
 form.addEventListener("submit", onFormSubmit);
 
@@ -7,5 +14,24 @@ function onFormSubmit(event) {
 	const data = new FormData(event.target);
 	const dataObject = Object.fromEntries(data.entries());
 	console.log(dataObject);
+	getNasaPhoto(dataObject.date);
 	form.reset();
+}
+
+function getNasaPhoto(date) {
+	const url = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`;
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			console.log(data)
+			showData(data)
+	})
+		.catch(error => console.error("API Error:", err))
+}
+
+function showData(data) {
+	result.innerHTML = `
+		<h3>${data.title}</h3>
+		<img src="${data.url}" alt="${data.title}" />
+`
 }
